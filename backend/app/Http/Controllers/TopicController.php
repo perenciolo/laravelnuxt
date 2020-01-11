4760ce46a9a70c580dc4d761665b19d485864e00
Gustavo Perenciolo <gustavoperenciolo@MacBook-Pro-de-Gustavo.local>
2020-01-10 21:56:45 -0300

    Add Topic and Post model|controller|resource|request

---
+++ backend/app/Http/Controllers/TopicController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\TopicCreateRequest;
+use App\Post;
+use App\Topic;
+use App\Http\Resources\Topic as TopicResource;
+
+class TopicController extends Controller
+{
+    public function store(TopicCreateRequest $request)
+    {
+        $topic = new Topic();
+        $topic->title = $request->title;
+        $topic->user()->associate($request->user());
+
+        $post = new Post();
+        $post->body = $request->body;
+        $post->user()->associate($request->user());
+
+        $topic->save();
+        $topic->posts()->save($post);
+
+        return new TopicResource($topic);
+    }
+}
--- backend/routes/api.php
+++ backend/routes/api.php
@@ -17,3 +17,7 @@ Route::post('/register', 'AuthController@register');
 Route::post('/login', 'AuthController@login');
 Route::post('/logout', 'AuthController@logout');
 Route::get('/user', 'AuthController@user');
+
+Route::group(['prefix' => 'topics'], function () {
+    Route::post('/', 'TopicController@store')->middleware('auth:api');
+});
